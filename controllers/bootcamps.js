de0a504acd1dabc45b8dee21e197ee738e1a3e84
Mackowi <dzierzenga.maciej@gmail.com>
2023-07-20 16:19:10 +0200

    pagination for bootcamps

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,15 +1,72 @@
 const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middleware/async')
+const geocoder = require('../utils/geocoder')
 const Bootcamp = require('../models/Bootcamp.js')
 
 // desc: get all bootcamps
 // route: GET /api/vi/bootcamps
 // methos: Public
 const getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find()
-  res
-    .status(200)
-    .json({ success: true, count: bootcamps.length, data: bootcamps })
+  // copy req.query
+  const reqQuery = { ...req.query }
+  // fields to exclude
+  const removeField = ['select', 'sort', 'page', 'limit']
+  // loop ofer removeField and delete them from reqQuery
+  removeField.forEach((param) => delete reqQuery[param])
+  //create query string
+  let queryStr = JSON.stringify(reqQuery)
+  // add $ in front of operators for mongodb queries
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, (match) => `$${match}`)
+  // creating query / finding resource
+  let query = Bootcamp.find(JSON.parse(queryStr))
+  // adding select field to the query
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ')
+    query = query.select(fields)
+  }
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ')
+    query = query.sort(sortBy)
+  } else {
+    query = query.sort('-createdAt')
+  }
+  // pagination
+  const page = parseInt(req.query.page, 10) || 1
+  const limit = parseInt(req.query.limit, 10) || 4
+  const startIndex = (page - 1) * limit
+  const endIndex = page * limit
+  const total = await Bootcamp.countDocuments()
+
+  query = query.skip(startIndex).limit(limit)
+
+  // executing query
+  const bootcamps = await query
+
+  // pagination results
+  const pagination = {}
+
+  // check if we're on the last page, if no then calculate the next page
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    }
+  }
+
+  // check if we're on the first page, if no then calculate the previous page
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    }
+  }
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    pagination,
+    data: bootcamps,
+  })
 })
 
 // desc: get bootcamp
@@ -65,10 +122,37 @@ const deleteBootcamp = asyncHandler(async (req, res, next) => {
   res.status(200).json({ success: true, data: {} })
 })
 
+// desc: Get bootcamps within a radius
+// route: GET /api/vi/bootcamps/radius/:zipcode/:distance
+// methos: Private
+const getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  const { zipcode, distance } = req.params
+
+  // get lat and lng from geocoder
+  const loc = await geocoder.geocode(zipcode)
+  const lat = loc[0].latitude
+  const lng = loc[0].longitude
+
+  // calculate the radius using radians
+  // divide distance by radius of earth
+  // Earth Radius = 6378km
+  const radius = distance / 6378
+
+  const bootcamps = await Bootcamp.find({
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  })
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps,
+  })
+})
+
 module.exports = {
   getBootcamps,
   getBootcamp,
   createBootcamp,
   updateBootcamp,
   deleteBootcamp,
+  getBootcampsInRadius,
 }
